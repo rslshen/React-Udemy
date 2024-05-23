61dba991318a7da41811cc4ec46ede2a84cc806c
rslshen <rosaline1129@163.com>
2024-05-23 15:44:10 -0400

    Completed workout timer project; Started Redux

--- 13-workout-timer/starter/src/Calculator.js
+++ 13-workout-timer/starter/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 const Calculator = ({ workouts, allowSound }) => {
@@ -7,15 +7,44 @@ const Calculator = ({ workouts, allowSound }) => {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak, allowSound]);
+
+  useEffect(() => {
+    const playSound = function () {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
+
+    playSound();
+  }, [duration, allowSound]);
+
+  useEffect(() => {
+    document.title = `Your ${number}-execise workout`;
+  }, [number, duration, sets]);
+
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
+
+  function handleInc() {
+    setDuration(Math.floor(duration + 1));
+  }
+
+  function handleDec() {
+    setDuration(duration > 1 ? Math.ceil(duration - 1) : 0);
+  }
 
   return (
     <>
@@ -66,13 +95,13 @@ const Calculator = ({ workouts, allowSound }) => {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && "0"}
           {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
