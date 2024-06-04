6991a4abddca4ee3a5f72e4c4246c40a65adcc59
rslshen <rosaline1129@163.com>
2024-06-04 12:15:06 -0400

    completed currency conversion

--- 15-redux-intro/starter/redux-intro/src/features/account/AccountOperations.js
+++ 15-redux-intro/starter/redux-intro/src/features/account/AccountOperations.js
@@ -14,14 +14,17 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((state) => state.account);
 
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
 
     setDepositAmount("");
+
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
@@ -64,8 +67,9 @@ function AccountOperations() {
             <option value="EUR">Euro</option>
             <option value="GBP">British Pound</option>
           </select>
-
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>{" "}
         </div>
 
         <div>
--- 15-redux-intro/starter/redux-intro/src/features/account/accountSlice.js
+++ 15-redux-intro/starter/redux-intro/src/features/account/accountSlice.js
@@ -2,6 +2,7 @@ const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialState, action) {
@@ -10,6 +11,7 @@ export default function accountReducer(state = initialState, action) {
       return {
         ...state,
         balance: state.balance + action.payload,
+        isLoading: false,
       };
     case "account/withdraw":
       return {
@@ -31,13 +33,29 @@ export default function accountReducer(state = initialState, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/loading":
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/loading" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 export function withdraw(amount) {
